46ed2ef58d3ef2334df69cc55064f218c3115934
lLija <ilija.trokovski@gmail.com>
2023-04-11 13:34:19 +0200

    todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
   return (
-    <div >
+    <div>
+      <Todos items={todos} />
     </div>
   );
 }
